1f2800d8298dcd0fcd2edd9546016725a533c865
Joko206 <12350110343@students.uin-suska.ac.id>
2025-05-13 15:19:07 +0700

    update cors

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/Joko206/UAS_PWEB1/database"
 	"github.com/Joko206/UAS_PWEB1/routes"
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -12,14 +11,6 @@ func main() {
 	database.GetDBConnection()
 
 	app := fiber.New()
-
-	app.Use(cors.New(cors.Config{
-		// Tentukan pengaturan CORS sesuai dengan kebutuhan
-		AllowOrigins:     "http://10.0.2.2:8000",         // Misalnya hanya dari domain ini yang boleh mengakses
-		AllowMethods:     "GET,POST,PUT,DELETE",          // Metode HTTP yang diizinkan
-		AllowHeaders:     "Origin, Content-Type, Accept", // Header yang diizinkan
-		AllowCredentials: true,                           // Mengizinkan pengiriman cookies
-	}))
 
 	routes.Setup(app)
 

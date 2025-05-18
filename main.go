2c1a4e3a27cf983cffcd85a8104857b81da139da
Joko206 <12350110343@students.uin-suska.ac.id>
2025-05-18 23:22:10 +0700

    menambah link vercel kedalam cors

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:5173",
+		AllowOrigins: "http://localhost:5173, https://brainquiz-psi.vercel.app/",
 		AllowMethods: strings.Join([]string{
 			fiber.MethodGet,
 			fiber.MethodPost,

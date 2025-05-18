f8aa1dac4d0a55a9bdaa0e7bbfeafb54b2b1e0d9
Joko206 <12350110343@students.uin-suska.ac.id>
2025-05-18 22:42:57 +0700

    add cors

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/Joko206/UAS_PWEB1/database"
 	"github.com/Joko206/UAS_PWEB1/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"strings"
 )
 
 func main() {
@@ -12,6 +14,19 @@ func main() {
 
 	app := fiber.New()
 
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "https://localhost:5173",
+		AllowMethods: strings.Join([]string{
+			fiber.MethodGet,
+			fiber.MethodPost,
+			fiber.MethodHead,
+			fiber.MethodPut,
+			fiber.MethodDelete,
+			fiber.MethodPatch,
+		}, ","),
+		AllowHeaders:     "",
+		AllowCredentials: true,
+	}))
 	routes.Setup(app)
 
 	app.Listen("0.0.0.0:8000")

0930323ed35c35fb1f7229b55b2ac513a484ecb5
Joko206 <12350110343@students.uin-suska.ac.id>
2025-06-07 18:04:33 +0700

    Updates database connection credentials
    Updates the database host, port, and password to reflect the new connection details.
    
    This ensures the application can connect to the correct database instance.

--- database/database.go
+++ database/database.go
@@ -8,10 +8,10 @@ import (
 )
 
 const (
-	host     = "metro.proxy.rlwy.net"
-	port     = 11951
+	host     = "ballast.proxy.rlwy.net"
+	port     = 46530
 	user     = "postgres"
-	password = "VxYgKiPnPDgILDlzcYAxXOzEdOTUQxwh"
+	password = "rQeJuOyOlkKxlVKfHYvhUkAoboRYCrWG"
 	dbname   = "railway"
 )
 
